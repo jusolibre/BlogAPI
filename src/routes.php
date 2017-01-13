39d6f6e1f030a688bc33734ec64cd75d8b89d9aa
xSlOom <illuclem@gmail.com>
2017-01-13 10:49:40 +0100

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -8,18 +8,58 @@
 */
 
 $app->get('/', function ($request, $response, $args) { // BLOG'S INDEX
-    return $this->renderer->render($response, 'index.phtml',$args);
+    return $response->getBody()->write("Hello!");
 });
 
 $app->get("/articles", function($request, $response) { // SHOW  ALL ARTICLES IN A JSON OBJECT
     $article    = [];
-    $query	= $this->db->query("select * from miniblog ORDER BY id DESC;");
-    $fetch	= $query->fetchAll();
+    $query	    = $this->db->query("SELECT * FROM miniblog ORDER BY id DESC;");
+    $fetch	    = $query->fetchAll();
     for ($i = 0; $i < sizeof($fetch); $i++) {
         $article[] = ["id" => $fetch[$i]["id"], "author" => $fetch[$i]["author"], "title" => $fetch[$i]["title"], "message" => $fetch[$i]["message"], "category" => $fetch[$i]["category"]];
     }
     $response->withHeader('Content-type', 'application/json');
     return $response->withJson(!empty($article) ? $article : ["error" => true, "message" => "Nothing found in database"], 200, JSON_PRETTY_PRINT);
+});
+
+$app->delete('/article/[{id}]', function($request, $response, $args) { // DELETE AN ARTICLE BY ID
+    if ((!isset($args["id"])) || (!is_numeric($args["id"]))) {
+        $data   = ["error" => true, "message" => "Argument id is missing or it's not numeric!"];
+    } else {
+        $id     = $args["id"];
+        $search = $this->db->query("SELECT id FROM miniblog WHERE id='{$id}'");
+        $fetch  = $search->fetch();
+        if (isset($fetch["id"])) {
+            $this->db->query("DELETE FROM miniblog WHERE id='{$id}'");
+            $data   = ["error" => false, "message" => "Article has been deleted!"];
+        } else {
+            $data   = ["error" => true, "message" => "Article not found"];
+        }
+    }
+    $response->withHeader('Content-type', 'application/json');
+    return $response->withJson($data, 200, JSON_PRETTY_PRINT);
+});
+
+$app->put('/article/[{id}]', function($request, $response, $args) { // UPDATE AN ARTICLE BY ID IF NUMERIC
+    $json       = json_decode($request->getBody(), true);
+    if (!is_numeric($args["id"])) {
+        $data = ["error" => true, "message" => "The id argument must be numeric."];
+    } else {
+        $id         = $args["id"];
+        $title      = isset($json["title"]) ? htmlspecialchars($json["title"]) : "";
+        $message    = isset($json["message"]) ? htmlspecialchars($json["message"]) : "";
+        $query      = $this->db->query("SELECT id FROM miniblog WHERE id='{$id}'");
+        $fetch      = $query->fetch();
+        if (isset($fetch["id"])) {
+            $this->db->query("update miniblog set title='{$title}', message='{$message}' where id='{$id}'");
+            $data   = ["error" => false, "message" => "Article has been updated!"];
+        } else {
+            $data   = ["error" => true, "message" => "Article not found"];
+        }
+    }
+    $response->withHeader('Content-type', 'application/json');
+    return $response->withJson($data, 200, JSON_PRETTY_PRINT);
+
 });
 
 $app->post('/article', function ($request, $response) { // SAVE A NEW ARTICLE FROM A JSON
@@ -38,7 +78,7 @@ $app->post('/article', function ($request, $response) { // SAVE A NEW ARTICLE FR
 		$message	= htmlspecialchars($json["message"]);
 		$author 	= htmlspecialchars($json["author"]);
 		
-		$register 	= $this->db->prepare("INSERT into miniblog(title, message, author, category, date) VALUES(:title, :message, :author, :category, :date)");
+		$register 	= $this->db->prepare("INSERT INTO miniblog(title, message, author, category, date) VALUES(:title, :message, :author, :category, :date)");
 		$register->execute(["title" => $title, "message" => $message, "author" => $author, "category" => "Informatic", "date" => time()]);
 		
 		$data = ["error" => false, "message" => "OK"];
@@ -53,7 +93,7 @@ $app->get('/search/[{id}]', function ($request, $response, $args) { // CHERCHER 
 	if ((!isset($args["id"])) || (!is_numeric($args["id"]))) {
 		$data	= ["error" => true, "message" => "ID is missing or it's not numeric.."];
 	} else {
-		$query	= $this->db->query("select * from miniblog where id='{$args["id"]}'");
+		$query	= $this->db->query("SELECT * FROM miniblog WHERE id='{$args["id"]}'");
 		$fetch	= $query->fetch();
 		$data	= isset($fetch["author"]) ? ["error" => false, "id" => (int)$fetch["id"], "author" => $fetch["author"], "title" => $fetch["title"], "message" => $fetch["message"]] : ["error" => true, "message" => "Article not found"];
 	}
@@ -66,5 +106,3 @@ $app->get('/search', function ($request, $response) {
     $response->withHeader('Content-type', 'application/json');
     return $response->withJson($data, 200, JSON_PRETTY_PRINT);
 });
-
-
