bddb6003d84ef67ecbf31202dcaf9671dc5b51a0
FoxyDecoy <royalkeiwurm@gmail.com>
2017-01-17 11:42:27 +0100

    static function

--- models/comments.class.php
+++ models/comments.class.php
@@ -1,48 +1,69 @@
 <?php
-class Comments {
 
-    public function select ($id) {
-        $query  = $this->db->query("SELECT * FROM comments WHERE article_id = '$id' ORDER BY date ASC;");
-        $fetch  = $query->fetch();
-        $row    = $query->rowCount();
-        $data   = ($row >= 1) ? $fetch : ["error" => true, "message" => "Not comments in database"];
-        return $data;
-    }
+    class Comments {
 
-    public function selectById ($id) {
-        if (!empty($id)) {
-            $query  = $this->db->query("SELECT * FROM comments WHERE id = $id");
-            $fetch  = $query->fetch();
-            $data   = $fetch == false ? ["error" => true, "message" => "Comment not found"] : $fetch;
-        } else {
-            $data = ["error" => true, "message" => "No ID selected"];
+        public static function select ($id) {
+
+            $query = DataBase::bdd()->query("SELECT * FROM comments WHERE article_id = '$id' ORDER BY date ASC;");
+            $fetch = $query->fetch();
+            $row = $query->rowCount();
+
+            $data = ($row >= 1) ? $fetch : $fetch = ["error" => true, "message" => "Not comments in database"];
+
+            return $data;
+
         }
-        return $data;
+
+        public static function selectById ($id) {
+
+            if (!empty($id)) {
+
+                $query = DataBase::bdd()->query("SELECT * FROM comments WHERE id = $id");
+                $fetch = $query->fetch();
+
+                if (!$fetch) {
+                    $data = ["error" => true, "message" => "Comment not found"];
+                }
+                else {
+                    $data = $fetch;
+                }
+            } else {
+                $data = ["error" => true, "message" => "Not id selected"];
+            }
+
+            return $data;
+        }
+
+        public static function deleteById ($id) {
+
+            $delete = DataBase::bdd()->query("DELETE FROM comments WHERE id = $id");
+            $data   = ["error" => false, "message" => "Comment has been deleted!"];
+
+            return $data;
+
+        }
+
+        public static function updateById ($author, $comment, $id) {
+
+            $update = DataBase::bdd()->query("UPDATE comments SET author='{$author}', comment='{$comment}' WHERE id='{$id}'");
+            $data   = ["error" => false, "message" => "Comment has been updated!"];
+
+            return $data;
+        }
+
+        public static function insert ($article_id, $author, $comment) {
+
+            $query = DataBase::bdd()->prepare("INSERT into comments(article_id, author, comment, date) VALUES(:article_id, :author, :comment, NOW())");
+
+            $query->bindParam(':article_id', $article_id);
+            $query->bindParam(':author', $author);
+            $query->bindParam(':comment', $comment);
+
+            $query->execute();
+
+            $data = ["error" => false, "message" => "Success"];
+
+            return $data;
+        }
+
     }
-
-    public function deleteById ($id) {
-        $delete = $this->db->query("DELETE FROM comments WHERE id = $id");
-        $data   = ["error" => false, "message" => "Comment has been deleted!"];
-        return $data;
-    }
-
-    public function updateById ($author, $comment, $id) {
-        $update = $this->db->query("UPDATE comments SET author='{$author}', comment='{$comment}' WHERE id='{$id}'");
-        $data   = ["error" => false, "message" => "Comment has been updated!"];
-        return $data;
-    }
-
-    public function insert ($article_id, $author, $comment) {
-        $query = $this->db->prepare("INSERT into comments(article_id, author, comment, date) VALUES(:article_id, :author, :comment, NOW())");
-
-        $query->bindParam(':article_id', $article_id);
-        $query->bindParam(':author', $author);
-        $query->bindParam(':comment', $comment);
-        $query->execute();
-
-        $data = ["error" => false, "message" => "Success"];
-
-        return $data;
-    }
-
-}
