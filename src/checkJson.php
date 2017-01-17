3a5c0d041f7056aa4cf3a45b4bad6c5cd19bfecd
Clément <illuclem@gmail.com>
2017-01-17 10:47:00 +0100

    Update checkJson.php

--- src/checkJson.php
+++ src/checkJson.php
@@ -1,19 +1,23 @@
 <?php
 
-  function checkJson($form, $json) {
-      $error = false;
+function checkJson($form, $json) {
+    $error = false;
+    foreach ($form as $key) {
+        /**
+         * If some arguments are missing, error is detected.
+         */
+        if (!isset($json[$key])) {
+            $error	= true;
+        }
 
-      foreach ($form as $key) {
-           if (!isset($json[$key])) {
-                $error	= true;
-           }
-
-          if (($key == "id" || $key == "article_id") && !is_numeric($json[$key])) {
-               $error = true;
-          }
-      }
-
+        /**
+         *  If the arg 'key' is found in the array ["id", "article_id"] but it's not numeric, error is detected.
+         */
+        if((in_array($key, ["id", "article_id"])) && (!is_numeric($json[$key]))) {
+            $error = true;
+        }
+    }
     return ($error);
-  }
+}
 
 ?>
