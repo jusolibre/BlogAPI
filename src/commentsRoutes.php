78cd549d2aaba5362679d293aefb80bee243b45f
Clément <illuclem@gmail.com>
2017-01-17 10:19:21 +0100

    Update commentsRoutes.php

--- src/commentsRoutes.php
+++ src/commentsRoutes.php
@@ -1,99 +1,99 @@
 <?php
 
-$app->post("/comments", function($request, $response) { // SHOW  ALL COMMENTS FROM THE ID ARTICLE IN A JSON OBJECT
-    $error = false;
-    $form = ["id"];
-    $json = json_decode($request->getBody(), true);
-
-    $error = checkJson($form, $json);
+/**
+ * @desc : This route will allow you to get a json with all comments from an article ID.
+ * @return : Json with all comments.
+ * @param : A json with "id" as argument. (Example : {"id": 1})
+ */
+$app->post("/comments", function($request, $response) {
+    $error  = false;
+    $form   = ["id"];
+    $json   = json_decode($request->getBody(), true);
+    $error  = checkJson($form, $json);
 
     if (!$error) {
         $fetch = Comments::select(htmlspecialchars($json["id"]));
-
         $response->withHeader('Content-type', 'application/json');
-        return $response->withJson(!empty($fetch) ? $fetch : ["error" => true, "message" => "Nothing found in database"], 200, JSON_PRETTY_PRINT);
+        $data   = !empty($fetch) ? $fetch : ["error" => true, "message" => "Nothing found in database"];
+    } else {
+        $data   = ["error" => true, "message" => "Something goes wrong. Did you sent all required arguments?"];
     }
-    else {
-        return $response->withJson(["error" => true, "message" => "Nothing found in database"], 200, JSON_PRETTY_PRINT);
-    }
+    $response->withHeader('Content-type', 'application/json');
+    return $response->withJson($data, 200, JSON_PRETTY_PRINT);
 });
 
-$app->delete("/comment", function($request, $response) { // DELETE THE COMMENT CORRESPONDING TO ID FROM THE JSON OBJECT
-    $error = false;
+/**
+ * @desc : This route will allow you to delete a comment from a choosen ID.
+ * @return : JSON with a boolean and a success/fail message.
+ * @param : A json with "id" as argument. (Example : {"id": 1})
+ */
+$app->delete("/comment", function($request, $response) {
+    $error  = false;
     $form	= ["id"];
     $json	= json_decode($request->getBody(), true);
-
-    $error = checkJson($form, $json);
+    $error  = checkJson($form, $json);
 
     if (!$error) {
-        $data = Comments::selectById(htmlspecialchars($json["id"]));
-
-        if (isset($data["id"])) {
-             $data = Comments::deleteByid(htmlspecialchars($json["id"]));
-        }
-        else {
-            $data = ["error" => true, "comment" => "Comment not found"];
-        }
-        $response->withHeader('Content-type', 'application/json');
-        return $response->withJson($data, 200, JSON_PRETTY_PRINT);
+        $data   = Comments::selectById(htmlspecialchars($json["id"]));
+        $data   = isset($data["id"]) ? Comments::deleteByid(htmlspecialchars($json["id"])) : ["error" => true, "message" => "Comment not found"];
+    } else {
+        $data   = ["error" => true, "message" => "Something goes wrong. Did you sent all required arguments?"];
     }
-    else {
-        return $response->withJson(["error" => true, "message" => "Nothing found in database"], 200, JSON_PRETTY_PRINT);
-    }
+    $response->withHeader('Content-type', 'application/json');
+    return $response->withJson($data, 200, JSON_PRETTY_PRINT);
 });
 
-$app->patch("/comment", function($request, $response) { // THIS ROUTE WILL ALLOW YOU TO MODIFY A COMMENT
-    $error = false;
+/**
+ * @desc : This route will allow you to update a comment.
+ * @return : JSON with boolean and a success/fail message.
+ * @param : A json with "comment", "id", "author" as arguments. (Example : {"id": 1, "comment": "hello", "author": "Clément"})
+ */
+$app->patch("/comment", function($request, $response) {
+    $error  = false;
     $form	= ["author", "comment", "id"];
     $json	= json_decode($request->getBody(), true);
-
-    $error = checkJson($form, $json);
+    $error  = checkJson($form, $json);
 
     if ($error == false) {
-
-      $comment = htmlspecialchars($json["comment"]);
-      $author = htmlspecialchars($json["author"]);
-      $id = htmlspecialchars($json["id"]);
+        $comment    = htmlspecialchars($json["comment"]);
+        $author     = htmlspecialchars($json["author"]);
+        $id         = htmlspecialchars($json["id"]);
 
       if (!$error) {
-         $data = Comments::selectById($id);
-
-         if (isset($data["id"])) {
-             $data = Comments::updateById($author, $comment, $id);
-         }
-         else {
-             $data = ["error" => true, "comment" => "comment not found"];
-         }
+         $data  = Comments::selectById($id);
+         $data  = isset($data["id"]) ? Comments::updateById($author, $comment, $id) : ["error" => true, "message" => "comment not found"];
       }
-
-      $response->withHeader('Content-type', 'application/json');
-      return $response->withJson($data, 200, JSON_PRETTY_PRINT);
+    } else {
+        $data   = ["error" => true, "message" => "Something goes wrong. Did you sent all required arguments?"];
     }
-    else {
-        return $response->withJson(["error" => true, "message" => "something went wrong in arguments"], 200, JSON_PRETTY_PRINT);
-    }
+    $response->withHeader('Content-type', 'application/json');
+    return $response->withJson($data, 200, JSON_PRETTY_PRINT);
 });
 
-$app->put("/comment", function($request, $response) { // ADD A COMMENT WITH DATA FROM A JSON OBJECT
-  $error	= false;
-  $form	= ["author", "article_id", "comment"];
-  $json	= json_decode($request->getBody(), true);
+/**
+ * @desc : This route will allow you to insert a new comment.
+ * @return : JSON with boolean and success/fail message.
+ * @param : A json with "id", "author", "article_id", "comment" as arguments. (Example : {"article_id":1, "author":"Clément", "comment": "Hello"})
+ */
+$app->put("/comment", function($request, $response) {
+    $error	= false;
+    $form	= ["author", "article_id", "comment"];
+    $json	= json_decode($request->getBody(), true);
+    $error  = checkJson($form, $json);
 
-  $error = checkJson($form, $json);
-
-  if ($error == false) {
-
-      $author		= htmlspecialchars($json["author"]);
-      $article_id	= htmlspecialchars($json["article_id"]);
-      $comment 	= htmlspecialchars($json["comment"]);
-
-      $data = Comments::insert($article_id, $author, $comment);
-
+    if (!$error) {
+        $data   = Articles::selectById($json["article_id"]);
+        if (isset($data["id"])) {
+            $author     = htmlspecialchars($json["author"]);
+            $article_id = htmlspecialchars($json["article_id"]);
+            $comment    = htmlspecialchars($json["comment"]);
+            $data       = Comments::insert($article_id, $author, $comment);
+        } else {
+            $data = ["error" => true, "message" => "Article not found"];
+        }
+	} else {
+		$data = ["error" => true, "message" => "Something goes wrong. Did you sent all required arguments?"];
 	}
-	else {
-		$data = ["error" => true, "message" => "Missing arguments in your JSON."];
-	}
-
 	$response->withHeader('Content-type', 'application/json');
 	return $response->withJson($data, 200, JSON_PRETTY_PRINT);
 });
